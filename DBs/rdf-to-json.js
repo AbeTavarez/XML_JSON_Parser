007d07acb90373492d97d2ca9c6a1ef8fcbb8678
Abraham E. Tavarez <abetavarez@icloud.com>
2020-10-08 21:46:38 -0400

    terminal interface

---
+++ DBs/rdf-to-json.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+// Parser
+const parseRDF = require('./lib/parse-rdf');
+// File
+const rdf = fs.readFileSync(process.argv[2]);
+//Parsing...
+const book = parseRDF(rdf);
+
+//* Logs Data Object withindentations
+console.log(JSON.stringify(book, null, ' '));
